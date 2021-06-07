de51d596ac2a1d55231bfb4ed61e088177af216f
Tan Nguyen <tan.nguyen.9225.1@gmail.com>
2021-06-06 18:39:06 -0700

    Student John and Sam added

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -4,5 +4,12 @@ public class Main {
 
     public static void main(String[] args) {
 	// write your code here
+        Student student1 = new Student("John", 2);
+        System.out.println("Student name: " + student1.getName() + " Student roll number: " + student1.getRoll_no());
+
+        Student student2 = new Student("Sam", 1);
+
+        student1.setAddress("2354 Main st, Sacramento 95768");
+        student2.setPhone_number("9163824075");
     }
 }
--- src/com/company/Student.java
+++ src/com/company/Student.java
@@ -1,5 +1,44 @@
 package com.company;
 
 public class Student {
+    private String name;
+    private int roll_no;
+    private String address;
+    private String phone_number;
+    public Student(String name, int roll_no){
+        this.name = name;
+        this.roll_no = roll_no;
+    }
 
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getRoll_no() {
+        return roll_no;
+    }
+
+    public void setRoll_no(int roll_no) {
+        this.roll_no = roll_no;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public String getPhone_number() {
+        return phone_number;
+    }
+
+    public void setPhone_number(String phone_number) {
+        this.phone_number = phone_number;
+    }
 }
